4be3a8b268b48f387f87097beff0dc83b38d4f75
Jeimmy Adrian Moreno Hernandez <adrianmorenojh@hotmail.com>
2023-05-10 22:12:58 -0400

    TwitterCard finalizada

--- projects/00-holaMundo/src/App.jsx
+++ projects/00-holaMundo/src/App.jsx
@@ -1,7 +1,43 @@
-export function App() {
+import "./App.css"
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'Adriano',
+    name: 'MAdriano Brazil',
+    isFollowing: true
+  },
+  {
+    userName: 'CR7',
+    name: 'PCristiano Ronaldo',
+    isFollowing: false
+  },
+  {
+    userName: 'leo',
+    name: 'Cuccitini',
+    isFollowing: true
+  },
+  {
+    userName: 'Vini',
+    name: 'vini',
+    isFollowing: false
+  }
+]
+
+export function App () {
   return (
-    <div>
-      <h1> Twitter Card</h1>
-    </div>
-  );
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
 }
